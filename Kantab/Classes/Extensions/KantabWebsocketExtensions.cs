3bf1796ed71c4ad7cb3fb0f3aaa1d272ae22ded3
GlitchyPSIX <11571952+GlitchyPSIX@users.noreply.github.com>
2025-05-11 22:51:46 -0400

    skip if ws is unreachable

--- Kantab/Classes/Extensions/KantabWebsocketExtensions.cs
+++ Kantab/Classes/Extensions/KantabWebsocketExtensions.cs
@@ -9,6 +9,7 @@ namespace Kantab.Classes.Extensions;
 
 public static class KantabWebsocketExtensions {
     public static async Task SendKantabMessage(this WebSocket ws, KantabMessage message) {
+        if (ws.State != WebSocketState.Open) return;
         await ws.SendAsync(new ArraySegment<byte>(message.ToBytes()), WebSocketMessageType.Binary, true, CancellationToken.None);
     }
 }
